e659fc85873d4dca09bad9a08173747d84c0aa1a
Abhinav Singh <abhinav8853490243@gmail.com>
2024-04-30 23:55:20 +0530

    GG

---
+++ frontend/react/src/main.jsx
@@ -0,0 +1,51 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { ToastContainer } from 'react-toastify';
+import { RouterProvider } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+import appState from './data/AppState';
+import routes from './routes';
+
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import Loading from './components/Loading';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+appState.__init__();
+
+// Initialize Firebase
+const app = initializeApp({
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+});
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <main className="font-poppins box-border smooth-scroll h-[100%] w-[100%] overflow-hidden">
+        <Suspense fallback={<Loading />}>
+          <RouterProvider router={routes} />
+        </Suspense>
+        <ToastContainer theme="dark" autoClose={1500} />
+        {/* ! USE FOR DEV MODE ONLY */}
+        <ReactQueryDevtools initialIsOpen={true} />
+      </main>
+    </GoogleOAuthProvider>
+  </QueryClientProvider>
+);
+
+// To use Firebase Storage:
+const storage = getStorage(app);
+export default storage;
---
+++ frontend/react/src/pages/AddItem/application/functions.js
@@ -0,0 +1,81 @@
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+
+import storage from '../../../main';
+
+/**
+ *  Uploads a file to Firebase Storage
+ * @param {File} file
+ * @returns {Promise<string?>} url
+ */
+export async function handleUpload(file) {
+  if (!file) {
+    alert('Please choose a file first!');
+  }
+
+  const _id = uuidv4();
+  const extension = file.name.split('.').pop();
+
+  const storageRef = ref(storage, `/files/${_id + extension}`);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  try {
+    // Wait for the upload task to complete
+    await new Promise((resolve, reject) => {
+      uploadTask.on('state_changed', null, reject, () => resolve());
+    });
+
+    // Get the download URL
+    const url = await getDownloadURL(uploadTask.snapshot.ref);
+    console.log(url);
+    return url;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+/**
+ * Saves data to backend
+ * @param {Object} data
+ * @param {string} data.listedBy - The ID of the user who listed the item.
+ * @param {string} data.name - The name of the item.
+ * @param {string} data.description - The description of the item.
+ * @param {string} data.price - The price of the item.
+ * @param {string} data.file - The file of the item.
+
+ * @returns {Promise<boolean>} true if successful, false otherwise
+ */
+
+export async function addItem(data) {
+  const { listedBy, name, description, price, file } = data;
+
+  try {
+    const img = await handleUpload(file);
+
+    const res = await axios.post(import.meta.env.VITE_API_URL + '/list/addItem', {
+      listedBy,
+      name,
+      description,
+      images: [img],
+      price,
+      comments: []
+    });
+
+    console.log(res);
+
+    if (res.data.statusCode === 200) {
+      toast.success(res.data.message);
+      return true;
+    } else {
+      toast.error(res.data.message);
+      return false;
+    }
+  } catch (err) {
+    console.log(err);
+    toast.error('Error uploading image');
+    return false;
+  }
+}
